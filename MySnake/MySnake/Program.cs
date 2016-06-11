c3091e35244dc0c6b90d924feefc6838bbb91632
Brandy Alexander <looneytunes1997@mail.ru>
2016-06-11 20:51:20 +0300

    Создан класс Point

---
+++ MySnake/MySnake/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Drow();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p1.Drow();
+
+        }
+    }
+}
