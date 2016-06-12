1ddca111725e483458bb616fcb4f3fb8e625f79b
Brandy Alexander <looneytunes1997@mail.ru>
2016-06-12 22:22:07 +0300

    Конструктор Point

--- MySnake/MySnake/Point.cs
+++ MySnake/MySnake/Point.cs
@@ -12,7 +12,14 @@ namespace MySnake
        public int y;
        public char sym;
 
-        public void Drow ()
+       public Point(int _x, int _y, char _sym)
+        {
+            x = _x;
+            y = _y;
+            sym = _sym;
+        }
+
+        public void Draw ()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
